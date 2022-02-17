c6d34435ff83d8819c665a3402fb5e47e715448e
youssef <youssefmohamedsabry36>
2022-02-17 23:03:02 +0200

    fix wallet

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -1,5 +1,4 @@
 import { PublicKey } from "@solana/web3.js"
 
 const wallet = process.env.WALLET_ID
-const MERCHANT_WALLE = new PublicKey(`${wallet}`)
-module.exports.wallet = MERCHANT_WALLE
+export const MERCHANT_WALLE = new PublicKey(`${wallet}`)
